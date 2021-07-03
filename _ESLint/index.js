26339feb7423053818d847494605ee5aa63fdf34
EvgeniiKolesnikov <jekow@yandex.ru>
2021-07-04 02:02:11 +0300

    Add ESLint rules

--- _ESLint/index.js
+++ _ESLint/index.js
@@ -1,10 +1,11 @@
 console.log('Hello, world');
 
 function addTitle() {
-  const title = document.querySelector('title').textContent
-  const body = document.querySelector('body')
-  body.insertAdjacentHTML("afterbegin", `<h1>${title}</h1>`)
+  const title = document.querySelector('title').textContent;
+  const body = document.querySelector('body');
+  body.insertAdjacentHTML('afterbegin', `<h1>${title}</h1>`);
 }
+addTitle();
 
-
-addTitle()
+// copy array
+// const itemsCopy = [...items];
