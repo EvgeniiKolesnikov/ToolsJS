eaab177fcbf5bc0bbc532b49f901a1c3200804af
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-06 10:04:31 +0300

    Add the third Webpack files

--- _Webpack/src/index.js
+++ _Webpack/src/index.js
@@ -1,5 +1,6 @@
-import Post from './Post'
-import webpackLogo from './assets/webpack-logo.png'
+import * as $ from 'jquery'
+import Post from '@models/Post'
+import webpackLogo from '@/assets/webpack-logo'
 import './styles/style.css'
 
 // import json from './assets/json'
@@ -17,7 +18,9 @@ function addTitle() {
 addTitle()
 
 const post = new Post('Webpack Post Title', webpackLogo);
-console.log('Post to String', post.toString());
+$('pre').addClass('code').html(post.toString())
+
+// console.log('Post to String', post.toString());
 
 // console.log('JSON:', json);
 // console.log('XML:', xml);
